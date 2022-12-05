bd8c9743c39ee4924182e8ba6af4302638bf0d06
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-12-05 12:01:04 -0300

    Test commit

--- get_all_links.py
+++ get_all_links.py
@@ -9,15 +9,15 @@ def return_all_videos(yt_api_key, channel_id):
         'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(
         yt_api_key, channel_id)
 
-    fileappend = open('all_links.txt', 'a')
-
     url = first_url
     while True:
         inp = urllib.request.urlopen(url, timeout=1)
         resp = json.load(inp)
         for i in resp['items']:
             if i['id']['kind'] == "youtube#video":
+                fileappend = open('all_links.txt', 'a')
                 fileappend.write(base_video_url + i['id']['videoId'] + '\n')
+                fileappend.close()
         try:
             next_page_token = resp['nextPageToken']
             url = first_url + '&pageToken={}'.format(next_page_token)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from get_all_links import return_all_videos
+from download_video_mp3 import download_video
 
 configfile = open('config.json')
 
@@ -11,3 +12,5 @@ yt_api_key = configjson['key']
 channel_id = configjson['channel_id']
 
 #return_all_videos(yt_api_key, channel_id)
+
+download_video()
