04e5583801d83247c5a9ffa795797eb7e52e1ac6
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-12-05 10:42:41 -0300

    Add channel_id, get variables from json file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import urllib
 import json
 
+configfile = open('config.json')
+
+configjson = json.load(configfile)
+
+configfile.close()
+
+yt_api_key = configjson['key']
+channel_id = configjson['channel_id']
